1261c55a80e4661c5f86ed3086b881e09ed171a8
Ben Drucker <bvdrucker@gmail.com>
2015-07-29 15:26:25 -0700

    Clarify var names in tests

--- test.js
+++ test.js
@@ -3,45 +3,45 @@
 var test = require('tape')
 var WeakmapEvent = require('./')
 
-test('returns geval event mapped to an object', function (t) {
+test('returns gevental eventent mapped to an object', function (t) {
   t.plan(1)
-  var ev = WeakmapEvent()
+  var event = WeakmapEvent()
   var obj = {}
 
-  var unlisten = ev.listen(obj, function (result) {
+  var unlisten = event.listen(obj, function (result) {
     t.equal(result, 'hello')
     unlisten()
   })
-  ev.broadcast(obj, 'hello')
+  event.broadcast(obj, 'hello')
 
   // This second broadcast is effectively a noop because of the unlisten
-  ev.broadcast(obj, 'hello')
+  event.broadcast(obj, 'hello')
 })
 
-test('dispatch events for an object', function (t) {
+test('dispatch eventents for an object', function (t) {
   t.plan(2)
-  var ev = WeakmapEvent()
+  var event = WeakmapEvent()
   var obj = {}
   var obj2 = {}
 
-  ev.listen(obj, function (data) {
+  event.listen(obj, function (data) {
     t.equal(data, 'hello')
   })
-  ev.listen(obj2, function (data) {
+  event.listen(obj2, function (data) {
     t.equal(data, 'goodbye')
   })
 
-  ev.broadcast(obj, 'hello')
-  ev.broadcast(obj2, 'goodbye')
+  event.broadcast(obj, 'hello')
+  event.broadcast(obj2, 'goodbye')
 })
 
 test('argument validation', function (t) {
-  var ev = WeakmapEvent()
+  var event = WeakmapEvent()
   t.throws(function () {
-    ev.listen(function listener () {})
+    event.listen(function listener () {})
   }, 'listen')
   t.throws(function () {
-    ev.broadcast('value')
+    event.broadcast('value')
   }, 'broadcast')
   t.end()
 })
